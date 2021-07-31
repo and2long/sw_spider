1a080493b5a1dd61f349742b2cad24e6f43988bb
and2long <and2long@gmail.com>
2021-07-31 12:17:00 +0800

    测试解析 cookie

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -41,9 +41,8 @@ void main() {
   });
 
   test('get title', () {
-    List<String> result = XPath.source(html)
-        .query('//*[@class="menu fl"]//li/a/@href')
-        .list();
+    List<String> result =
+        XPath.source(html).query('//*[@class="menu fl"]//li/a/@href').list();
     print(result);
   });
 
@@ -61,5 +60,23 @@ void main() {
 
   test('date', () {
     print(DateTime.now().toString().substring(0, 10));
+  });
+
+  test('get cookie', () {
+    String setCookie =
+        "XSRF-TOKEN=eyJpdiI6Ik95UEtiY2xsOHhYZmcrY0JLNnpKRnc9PSIsInZhbHVlIjoiZXczVklVcThiaGZ5Y2lQMjArZjgzeUY5Q29HdGNmMDJ5dHNxdmk3QnBydU1hOFVwOElaT3pjMHBUV3ByRFI1QiIsIm1hYyI6IjFmMzgwMWQxOGQyMDFhZjY3ZDkzZjFlNWE4ZDIwMGJmN2E0N2JlZTYxMjYxOTc0MzFiNTJhODNlMzFjODJkMDYifQ%3D%3D; expires=Sat, 31-Jul-2021 04:45:40 GMT; Max-Age=7200; path=/";
+    RegExp rxToken = RegExp(r'XSRF-TOKEN=\s*([\s\S]*?)\s*;');
+    RegExpMatch? matchToken = rxToken.firstMatch(setCookie);
+    if (matchToken != null) {
+      print(matchToken.group(1));
+    }
+
+    String setCookie1 =
+        'alps_session=eyJpdiI6ImU3d1ZLckpraGJDU1B3OUFTMkh4eEE9PSIsInZhbHVlIjoienppNXpTTXJISE04VXhja1FIRVlpaVNGVTM1dVhIN2x3TDczTDhMMldZemUrRXMycThEaWxlcXNjdDd5V2xScCIsIm1hYyI6ImZmMmNmMDVkMjYxMTdhYjI4OGQ3MzU3OTBmMThkZjJiYWE1YzI2YzVmZDIzZjI2MTIyYThmNmNjMTM2YjJmNzkifQ%3D%3D; expires=Sat, 31-Jul-2021 04:28:42 GMT; Max-Age=7200; path=/; httponly';
+    RegExp rxSession = RegExp(r'alps_session=\s*([\s\S]*?)\s*;');
+    RegExpMatch? matchSession = rxSession.firstMatch(setCookie1);
+    if (matchSession != null) {
+      print(matchSession.group(1));
+    }
   });
 }
